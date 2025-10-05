9d58540d733d01761c8d98fbffcd20607671bc06
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-10-04 21:32:30 -0700

    cleanup czip build script

--- scripts/czip.sh
+++ scripts/czip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CZIP_SRC=etc/czip.c
+
 if [ "${CFLAGS}" = "" ]; then
 	CFLAGS="-O3 -flto"
 fi
@@ -8,6 +10,13 @@ if [ "${CC}" = "" ]; then
 fi
 OUT_BIN="./target/bin/czip"
 
-COMMAND="${CC} -Wno-pointer-sign -Isrc/include etc/czip.c ${CFLAGS} -o ${OUT_BIN} -lfam -Ltarget/lib"
-echo ${COMMAND}
-${COMMAND}
+if [ ! -e ${OUT_BIN} ] || [ ${CZIP_SRC} -nt ${OUT_BIN} ]; then
+	COMMAND="${CC} \
+		-Wno-pointer-sign \
+		-Isrc/include ${CZIP_SRC} \
+		${CFLAGS} \
+		-o ${OUT_BIN} \
+		-lfam -Ltarget/lib"
+	echo ${COMMAND}
+	${COMMAND}
+fi
