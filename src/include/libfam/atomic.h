ee7db3720a1a95fa44206a58c868f40679a974b1
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-10-04 18:36:45 -0700

    compare exchange arm

--- src/include/libfam/atomic.h
+++ src/include/libfam/atomic.h
@@ -29,8 +29,35 @@
 #include <libfam/types.h>
 
 static __inline i32 __cas32(u32 *ptr, u32 *expected, u32 desired) {
+#ifdef __x86_64__
 	return __atomic_compare_exchange_n(ptr, expected, desired, false,
 					   __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+#elif defined(__aarch64__)
+	u32 result;
+	i32 success;
+	u32 orig_expected = *expected;
+	i32 retries = 5;
+	while (retries--) {
+		__asm__ volatile(
+		    "ldaxr %w0, [%2]\n"	   /* Load-exclusive 32-bit */
+		    "cmp %w0, %w3\n"	   /* Compare with *expected */
+		    "b.ne 1f\n"		   /* Jump to fail if not equal */
+		    "stxr w1, %w4, [%2]\n" /* Store-exclusive desired */
+		    "cbz w1, 2f\n"     /* Jump to success if store succeeded */
+		    "1: mov %w1, #0\n" /* Set success = 0 (fail) */
+		    "b 3f\n"
+		    "2: mov %w1, #1\n" /* Set success = 1 (success) */
+		    "3: dmb ish\n"     /* Memory barrier */
+		    : "=&r"(result), "=&r"(success)
+		    : "r"(a), "r"(*expected), "r"(desired)
+		    : "w1", "memory" /* Use w1 instead of x0, x1 */
+		);
+		if (success) break;
+		*expected = result;
+		if (result != orig_expected) break;
+	}
+	return success;
+#endif /* __aarch64__ */
 }
 
 static __inline u32 __aand32(volatile u32 *ptr, u32 value) {
