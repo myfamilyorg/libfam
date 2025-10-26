d8c0d18b5caa11e56da9be9a69e09bcce1ae42e3
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-10-25 23:48:17 -0700

    move to 10 bit max len

--- src/include/libfam/compress_impl.h
+++ src/include/libfam/compress_impl.h
@@ -30,10 +30,10 @@
 #include <libfam/limits.h>
 #include <libfam/types.h>
 
-#define REPEAT_VALUE_INDEX 10
-#define REPEAT_ZERO_LONG_INDEX 11
-#define REPEAT_ZERO_SHORT_INDEX 12
-#define MAX_CODE_LENGTH 9
+#define REPEAT_VALUE_INDEX 11
+#define REPEAT_ZERO_LONG_INDEX 12
+#define REPEAT_ZERO_SHORT_INDEX 13
+#define MAX_CODE_LENGTH 10
 #define MAX_BOOK_CODE_LENGTH 7
 #define MAX_BOOK_CODES (MAX_CODE_LENGTH + 4)
 #define LEN_SHIFT 4
