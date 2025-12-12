5840799edc3a0b741f53e9ece55422c5b6eb7308
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-12-12 11:33:35 -0800

    fix gcc issue

--- src/include/dilithium/sign.h
+++ src/include/dilithium/sign.h
@@ -9,27 +9,8 @@
 
 int crypto_sign_keypair(uint8_t *pk, uint8_t *sk);
 
-int crypto_sign_signature_internal(uint8_t *sig, size_t *siglen,
-				   const uint8_t *m, size_t mlen,
-				   const uint8_t *pre, size_t prelen,
-				   const uint8_t rnd[RNDBYTES],
-				   const uint8_t *sk);
-
-int crypto_sign_signature(uint8_t *sig, size_t *siglen, const uint8_t *m,
-			  size_t mlen, const uint8_t *ctx, size_t ctxlen,
-			  const uint8_t *sk);
-
 int crypto_sign(uint8_t *sm, size_t *smlen, const uint8_t *m, size_t mlen,
 		const uint8_t *ctx, size_t ctxlen, const uint8_t *sk);
-
-int crypto_sign_verify_internal(const uint8_t *sig, size_t siglen,
-				const uint8_t *m, size_t mlen,
-				const uint8_t *pre, size_t prelen,
-				const uint8_t *pk);
-
-int crypto_sign_verify(const uint8_t *sig, size_t siglen, const uint8_t *m,
-		       size_t mlen, const uint8_t *ctx, size_t ctxlen,
-		       const uint8_t *pk);
 
 int crypto_sign_open(uint8_t *m, size_t *mlen, const uint8_t *sm, size_t smlen,
 		     const uint8_t *ctx, size_t ctxlen, const uint8_t *pk);
