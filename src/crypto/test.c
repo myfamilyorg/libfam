eec2dd9d3577adf9d63cca87f2ce82efbdaca665
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-12-13 00:26:36 -0800

    make valgrind happy

--- src/crypto/test.c
+++ src/crypto/test.c
@@ -55,7 +55,7 @@ Test(aighthash) {
 Test(twobytefails) {
 	u32 h1 = aighthash32("a\0", 2, 0);  // input: 0x61 0x00
 	u32 h2 = aighthash32("ab", 2, 0);   // input: 0x61 0x62
-					   // println("h1={x},h2={x}", h1, h2);
+					    // println("h1={x},h2={x}", h1, h2);
 
 	ASSERT(h1 != h2, "twobyte");
 }
@@ -676,7 +676,7 @@ Test(dilithium) {
 
 Test(dilithium_perf) {
 	__attribute__((aligned(32))) u8 rnd[SEEDLEN] = {0};
-	Message m;
+	Message m = {0};
 	SecretKey sk;
 	PublicKey pk;
 	Signature sm;
